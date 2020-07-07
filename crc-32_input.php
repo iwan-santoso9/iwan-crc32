2972001bd7fa20c310ffb4aaa6308d2d7ba1f000
iwan-santoso9 <kenzoiwana@gmail.com>
2020-07-08 06:48:42 +0700

    initial commit

--- crc-32_input.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$namaFile = explode('\\', __FILE__);
-print_r('Nama File : '.(end($namaFile)));
-echo "\n"; 
-echo "Masukkan Heksa :";
-$x = trim(fgets(STDIN));
-$checksum = crc32($x);
-printf('Output Heksa : '."%x\n", $checksum); 
- ?>
